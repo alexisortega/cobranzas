d4f62d8bc9de3d6bbb02d71b08ef6cfe3d77fc48
alexis ortega <alebdi_ort@hotmail.com>
2022-10-19 16:21:35 -0500

    se agrego unas configuraciones y se inicializo servicio de firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'ui/onboarding_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
